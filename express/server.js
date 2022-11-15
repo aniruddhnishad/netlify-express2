934b4b91a4bd945af682b1b633f46adf5017096a
aniruddhnishad <mr.aniruddhnishad@gmail.com>
2022-11-16 00:45:34 +0530

    fv

--- express/server.js
+++ express/server.js
@@ -7,8 +7,6 @@ const router = express.Router();
 
 app.use(bodyParser.json());
 
-app.use('/.netlify/functions/server', router); 
-
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
@@ -17,6 +15,8 @@ router.get('/', (req, res) => {
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use('/.netlify/functions/server', router); 
 
 module.exports = app;
 module.exports.handler = serverless(app);
